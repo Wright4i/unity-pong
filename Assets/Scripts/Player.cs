f9830adccfb39583a940c2d6cf0f2fe0d0046377
Wright4i <joseph@wright400.com>
2022-09-13 14:26:50 -0700

    Bugfix - Limit player paddle to same limits as CPU

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,9 +16,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -7.75f) {
             transform.position += new Vector3(0f, (moveSpeed * -1) * Time.deltaTime, 0f);
-        } else if (Input.GetKey(KeyCode.UpArrow)) {
+        } else if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 7.75f) {
             transform.position += new Vector3(0f, moveSpeed * Time.deltaTime, 0f);
         }
     }
